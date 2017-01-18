0cca43ca577444fd9d5f40fdd5465c5c05e457a8
Hoshi Yamazaki <hoshiyamazaki01@gmail.com>
2017-01-18 20:28:58 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>VoxTale0</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/index.css" type="text/css">
+        <link rel="stylesheet" href="css/snow.css" type="text/css">
+    </head>
+
+    <body>
+        <div id="snow">
+            <div id="box">
+                    VoxTale0<br>
+                    <form action="login.php" method="POST">
+                    <input type="text" name="log" value="Login">
+                    <input type="text" name="pass" value="Password">
+                    <input type="submit" value="Get into!">
+                    </form>
+                    <div id="small">
+                        <a href="register.php">Not registered? Click here</a>
+                    </div>
+            </div>
+        </div>
+    </body>
+</html>
