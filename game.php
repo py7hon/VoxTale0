8073fd3b5cd577198eaebce14083a694da6a39ec
Hoshi Yamazaki <hoshiyamazaki01@gmail.com>
2017-01-19 20:43:16 +0100

    Basic game scheme

---
+++ game.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>VoxTale0</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/game.css" type="text/css">
+        <link rel="stylesheet" href="css/snow.css" type="text/css">
+    </head>
+
+    <body>
+        <div id="snow">
+            <div id="title">
+                VoxTale0
+            </div>
+            
+            <div id="container">
+                <div id="menu">
+                </div>
+
+                <div id="right">
+                </div>
+
+                <div id="content">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
